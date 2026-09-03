0c4d737c2a2b8d749cfbd227608009d8c753501f
alijonzardov97-cmyk <alijonzardov97@gmail.com>
2026-09-03 09:54:01 +0300

    Update MainActivity.kt

--- app/src/main/java/com/alijon/detector/MainActivity.kt
+++ app/src/main/java/com/alijon/detector/MainActivity.kt
@@ -41,7 +41,7 @@ import kotlinx.coroutines.launch
 /* ------------------------------------------------------------------ настройка */
 
 /** Репозиторий с прошивками: владелец/имя. */
-private const val REPO = "alijon-pi-detector"
+private const val REPO = "alijonzardov97-cmyk/detector-app"
 
 /* -------------------------------------------------------------------- палитра */
 
