5d91a5d755baf7539aef39dd4818660a24e88935
alijonzardov97-cmyk <alijonzardov97@gmail.com>
2026-09-03 15:36:06 +0300

    Add files via upload

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.alijon.detector"
         minSdk = 26              // Android 8: BLE + фоновая работа без плясок
         targetSdk = 35
-        versionCode = 3
-        versionName = "1.2"
+        versionCode = 4
+        versionName = "1.3"
     }
 
     buildTypes {
